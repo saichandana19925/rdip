3404a07159a53b65f8b562455c646986dc6b9f91
saichandana19925 <59165702+saichandana19925@users.noreply.github.com>
2020-07-06 09:08:35 +0530

    js and js query

--- task.js
+++ task.js
@@ -13,14 +13,6 @@ calculatorForm.evalresult.value=t
 function abs(result)
 {
 	calculatorForm.evalresult.value=Math.abs(result)
-}
-	function Palindrome()
-	{
-		var rem, temp, final = 0;
-		var number = Number(document.getElementById("N").value);
-
-		temp = number;
-		while(number>0)
 		{
 			rem = number%10;
 			number = parseInt(number/10);
@@ -29,51 +21,6 @@ function abs(result)
 		if(final==temp)
 		{
 			window.alert("The inputed number is Palindrome");
-		}
-		else
-		{
-			window.alert("The inputted number is not palindrome");
-		}
-	}
-var button = document.getElementById('button-test');
-button.addEventListener('click', checkAnagram);
-var resultDiv = document.getElementById('result');
-
-function checkAnagram() {
-  var str1 = document.getElementById('string1').value;
-  var str2 = document.getElementById('string2').value;
-  if(str1 !== null && str2 !== null) {
-    if(str1.length !== str2.length) {
-      resultDiv.innerHTML = "Strings are not anagrams.";
-      return false;
-    }
-    var hashTable = {};
-    for(var i = 0; i < str1.length; i++) {
-      if(hashTable.hasOwnProperty(str1[i])) {
-        hashTable[str1[i]] = hashTable[str1[i]] + 1;
-    } else {
-     hashTable[str1[i]] = 1; 
-    }
-  }
-   
-  for(var i = 0; i < str2.length; i++) {
-    if(hashTable.hasOwnProperty(str2[i])) {
-      hashTable[str2[i]] = hashTable[str2[i]] - 1;
-    } else {
-      resultDiv.innerHTML = "Strings are not anagrams.";
-      return false;
-    }
-  }
-    
-  for(var i in hashTable) {
-    if(hashTable[i] !== 0) {
-      resultDiv.innerHTML = "Strings are not anagrams.";
-      return false;
-    } else {
-       resultDiv.innerHTML = "Strings are anagrams!";
-    }
-  }
-}
 }
 function validate() {
   var no = document.getElementById("no").value;
@@ -91,3 +38,93 @@ function validate() {
     alert("Please enter a valid e-mail address:");
     return false;
   }
+ function palin()
+{
+var palindrome=document.getElementById("pal").value;
+
+var rev=reverseString(palindrome);
+
+if(rev===palindrome){
+alert("string is palindrome");
+}
+else{
+alert("not a palindrome");
+}
+return false;
+}
+
+function reverseString(str) {
+
+var splitString = str.split("");
+var reverseArray = splitString.reverse();
+var joinArray = reverseArray.join("");
+    return joinArray;
+}
+
+
+function anagram()
+{
+var S1=document.getElementById("string1").value;
+var S2=document.getElementById("string2").value;
+//alert(S1+" "+S2);
+ var str1=S1.replace(/\s+/g,"");
+ //alert(str1);
+ var str2=S2.replace(/\s+/g,"");
+ //alert(str2);
+ 
+var con1=str1.toLowerCase();
+var con2=str2.toLowerCase();
+
+var arr1=con1.split("");
+var arr2=con2.split("");
+arr1.sort();
+arr2.sort();
+//alert(arr1+" "+arr2);
+ var st1=arr1.join("");
+ var st2=arr2.join("");
+ //alert(st1+" "+st2);
+if(st1==st2)
+{
+alert("anagrams");
+}
+else
+{
+alert("not anagrams");
+}
+
+return false;
+
+}
+
+
+function rand()
+{
+var r1= Math.floor(Math.random() * (+1000 - +0) + +0);
+var r2= Math.floor(Math.random() * (+1000 - +0) + +0);
+
+document.getElementById("num1").value=r1;
+document.getElementById("num2").value=r2;
+
+var rem1=r1%3;
+var rem2=r2%3;
+if(rem1==0 && rem2==1 || rem2==0 && rem1==1)
+{
+
+document.getElementById("result").value="human survives";
+}
+else if(rem1==rem2)
+{
+document.getElementById("result").value="tie";
+}
+else if(rem1==1 && rem2==2 || rem2==1 && rem1==2)
+{
+document.getElementById("result").value= "Cockroach survives";
+}
+else if(rem1==0 && rem2==2 || rem2==0 && rem1==2)
+{
+document.getElementById("result").value="human dies";
+}
+
+return false;
+}
+
